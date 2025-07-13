a1e32ff71d52e009229e0b6e6eb53ccebec58dd7
Christopher <ckaliszewski@gmail.com>
2025-07-13 02:39:19 -0400

    move all components to use dataclass decorator

--- components.py
+++ components.py
@@ -1,24 +1,27 @@
 from abc import ABC
 from dataclasses import dataclass
 
-class Component(ABC): pass
+class Component(ABC):
+    pass
 
-class Collider(Component): pass
+class Collider(Component):
+    pass
 
+@dataclass
 class Color(Component):
-    def __init__(self, color: tuple[int, int, int]):
-        self.color = color
+    color: tuple[int, int, int]
 
+@dataclass
 class Name(Component):
-    def __init__(self, name: str):
-        self.name = name
+    name: str
 
+@dataclass
 class Position(Component):
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+    x: float
+    y: float
 
-class Shape(Component): pass
+class Shape(Component):
+    pass
 
 @dataclass
 class Circle(Shape):
@@ -33,7 +36,7 @@ class Rectangle(Shape):
         yield self.width
         yield self.height
 
+@dataclass
 class Velocity(Component):
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+    x: float
+    y: float
